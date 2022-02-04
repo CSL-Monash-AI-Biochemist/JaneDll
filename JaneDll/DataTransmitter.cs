c7c90ec72c3230b20b3980d2e6b37886e1e59c33
calvong <calvongchi@gmail.com>
2022-02-04 17:49:28 +1100

    Update DataTransmitter.cs

--- JaneDll/DataTransmitter.cs
+++ JaneDll/DataTransmitter.cs
@@ -11,28 +11,29 @@ namespace JaneDll
 {
     public class DataTransmitter 
     {
+        private const string pyScriptPath = 
+            "C:\\code\\jane_command_centre\\momentum_interface\\data_exchange_client.py";
+
         public DataTransmitter(){}
 
-        public string SendData(int mmt_data, out int is_outlier, out int temperature)
+        public string updateState(string mmt_state)
         {
             // Initialising python engine -----------------------------------------------
             var engine = Python.CreateEngine();
 
             // reading python code from file
-            var python_source = engine.CreateScriptSourceFromFile("C:\\code\\CSL\\Momentum_data_exchange_server\\data_exchange_server.py");
+            //var python_source = engine.CreateScriptSourceFromFile("C:\\code\\Momentum_data_exchange_server\\data_exchange_server.py");
+            var python_source = engine.CreateScriptSourceFromFile(pyScriptPath);
 
             var python_scope = engine.CreateScope();
             python_source.Execute(python_scope);
 
-            var classDataExchangeServer = python_scope.GetVariable("DataExchangeServer");
-            var dataExchange = engine.Operations.CreateInstance(classDataExchangeServer);
+            var classDataExchangeClient = python_scope.GetVariable("DataExchangeClient");
+            var dataExchange = engine.Operations.CreateInstance(classDataExchangeClient);
             // --------------------------------------------------------------------------
 
-            var python_data = dataExchange.send_data(mmt_data);
-
-            is_outlier = python_data[0];
-            temperature = python_data[1];
-
+            var python_data = dataExchange.update_mmt_state(mmt_state);
+            
             return "ok";
         }
 
